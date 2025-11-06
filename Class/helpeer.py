872b5948aeb4274d844ba99a6772294ebf1def56
sGhouse5 <sghouse77801@gmail.com>
2025-11-06 10:01:58 +0530

    added updated files

---
+++ Class/helpeer.py
@@ -0,0 +1,21 @@
+import os
+# Module example 
+
+def calcCircumference(radius: float) -> float:
+    """
+    caculate the circumference based on the radius
+    """
+    return 2 * (22/7) * radius 
+
+def calAreaOfCircle(radius=None, diameter=None):
+    """
+    return area of circle based on radius or diameter
+    """
+    pi = 22/7
+    if radius is not None:
+        return pi * radius ** 2
+    elif diameter is not None:
+        return pi * (diameter/2) ** 2
+    
+ 
+os.getcwd()
